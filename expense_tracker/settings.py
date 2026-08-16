06d81540e75dc7cbbb1d4049a1363f1480bc0862
robotujjawal <ujjawalmehtasingoli@gmail.com>
2026-08-17 02:05:37 +0530

    Configure Django for Render

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -9,9 +9,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-change-this-key-before-deploying-to-production'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+import os
 
-ALLOWED_HOSTS = ['*']
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
